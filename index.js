45ce5c3ff5ec48bbac8031abe9cb22f89b2b098e
Vinh Tran <vinhjs@gmail.com>
2018-11-22 15:04:29 +0700

    remove ssl

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var app = express();
-var fs = require('fs');
 var ExpressPeerServer = require('peer').ExpressPeerServer;
 var options = {
     debug: true
@@ -8,10 +7,7 @@ var options = {
 app.get('/', function(req, res, next) { res.send('Hello world!'); });
 
 
-var server = require('https').createServer({
-    key: fs.readFileSync('./certificates/key.pem', 'utf8'),
-    cert: fs.readFileSync('./certificates/cert.pem', 'utf8')
-}, app);
+var server = require('https').createServer(app);
 var peerserver = ExpressPeerServer(server, options);
 
 app.use('/peerjs', peerserver);
